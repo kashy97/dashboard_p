021ad1c0246054a5b173cfb6076cc36ae18e6222
unknown <admin@D-6099.pmplcorp.com>
2022-02-25 16:06:58 +0530

    Added full table

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -4,59 +4,60 @@ import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 const PageTable = (props: any) => {
   // const [diff, setDiff] = useState();
   const [tableData, setTableData] = useState([]);
+
   const columns = [
     { field: "date", headerName: "Start_Date", width: 150 },
     { field: "region", headerName: "Region", width: 200 },
     { field: "store", headerName: "Store", width: 200 },
     { field: "review", headerName: "Review", width: 100 },
     { field: "rating", headerName: "Rating", width: 100 },
-    // { field: "end_date", headerName: "End_Date", width: 150 },
-    // { field: "end_review", headerName: "Review", width: 100 },
-    // { field: "end_rating", headerName: "Rating", width: 100 },
-    // {
-    //   field: "diff",
-    //   headerName: "Diff",
-    //   width: 100,
-    // },
+    { field: "end_date", headerName: "End_Date", width: 150 },
+    { field: "end_review", headerName: "Review", width: 100 },
+    { field: "end_rating", headerName: "Rating", width: 100 },
+    {
+      field: "diff",
+      headerName: "Diff",
+      width: 100,
+      valueGetter: (params: any) =>
+      params.getValue(params.id, "review") + params.getValue(params.id, "end_review"),
+    },
   ];
 
   useEffect(() => {
-    // if (props.click === true) {
-    console.log("button worked");
+    console.log("button worked")
     fetch("")
       .then((data) => data.json())
       .then((data) => setTableData(data));
-    // }
-  }, []);
+  }, [props.value]);
 
   return (
     <>
       <div className="tables">
-        { (props.click===true)? 
-        <DataGrid
-          rows={tableData}
-          columns={columns}
-          pageSize={12}
-          components={{
-            Toolbar: GridToolbar,
-          }}
-          initialState={{
-            filter: {
-              filterModel: {
-                items: [
-                  {
-                    columnField: "date",
-                    id: "70958",
-                    operatorValue: "equals",
-                    value: props.value[0],
-                  },
-                ],
+        { props.click===true ?
+          <DataGrid
+            rows={tableData}
+            columns={columns}  
+            pageSize={12}
+            components={{
+              Toolbar: GridToolbar,
+            }}
+            initialState={{
+              filter: {
+                filterModel: {
+                  items: [
+                    {
+                      columnField: "date",
+                      id: "70958",
+                      operatorValue: "equals",
+                      value: props.value[0],
+                    },
+                  ],
+                },
               },
-            },
-          }}
-        />
-        : <></>
-       } 
+            }}
+          />
+          : <></>
+        }
       </div>
     </>
   );
